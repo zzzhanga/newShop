5172c1ac10e5cd5a7bb739341651050f3206ae62
your name <your email>
2020-06-14 18:15:15 +0800

    hang，框架完成

---
+++ newShop/src/pages/Search/search.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+Search
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ newShop/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Miste from '../pages/Miste/Miste'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Search from '@/pages/Search/Search.vue'
+import Login from '@/pages/Login/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:"history",
+  routes: [
+    {
+      path:"/",
+      redirect:"/miste"
+    },
+    {
+      path: '/miste',
+      name: 'miste',
+      component: Miste
+    },{
+      path:'/order',
+      name:'order',
+      component:Order
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      component: Login
+    },{
+      path:'*',
+      component:Miste
+    }
+  ]
+})
